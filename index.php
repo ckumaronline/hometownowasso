8ba9766403ef6951289fe4cbb1b26b5265f94113
CKUMAR <ckumaronline@users.noreply.github.com>
2018-04-09 23:11:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-Hellow World :)
+<?php
+   $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = @$_SERVER['REMOTE_ADDR'];
+    $result  = array('country'=>'', 'city'=>'');
+    if(filter_var($client, FILTER_VALIDATE_IP)){
+        $ip = $client;
+    }elseif(filter_var($forward, FILTER_VALIDATE_IP)){
+        $ip = $forward;
+    }else{
+        $ip = $remote;
+    }
+    $ip_data = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));    
+    
+
+ $countryCode = $ip_data->geoplugin_countryCode;
+  
+ if($countryCode != "US")
+ {
+  header('Location: http://www.hometownowasso.com/');
+ }
+
+?>
